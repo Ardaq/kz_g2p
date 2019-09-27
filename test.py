ca5fd6af9eb46a74f85e30db112361b053885c6a
Ardager <ardager@163.com>
2019-09-27 19:30:54 +0800

    test unit

---
+++ test.py
@@ -0,0 +1,21 @@
+import unittest
+from kzphoneme import Phoneme
+
+arab_text = 'ءبىلىم بۇلاعى'
+cyril_text = 'білім бұлағы'
+
+
+class PhonemeTestCase(unittest.TestCase):
+    pho = Phoneme()
+
+    def test_phoneme_arab(self):
+        arab_res = self.pho(arab_text, 'arab')
+        self.assertEqual(arab_res, ['B', 'IH', 'L', 'IH', 'M', ' ', 'B', 'U', 'L', 'A', 'C', 'I'])
+
+    def test_phoneme_cyril(self):
+        cyril_res = self.pho(cyril_text, 'cyril')
+        self.assertEqual(cyril_res, ['B', 'IH', 'L', 'IH', 'M', ' ', 'B', 'U', 'L', 'A', 'C', 'I'])
+
+
+if __name__ == '__main__':
+    unittest.main()
