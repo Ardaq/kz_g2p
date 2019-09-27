1710cb48043275937f199417354785471e5ae886
Ardager <ardager@163.com>
2019-09-27 19:15:51 +0800

    create script convert class

---
+++ kzphoneme/convert.py
@@ -0,0 +1,9 @@
+import os
+import re
+
+dirname = os.path.dirname(__file__)
+
+
+class Convert(object):
+    def __init__(self, *args, **kwargs):
+        super(Convert, self).__init__(*args, **kwargs)
