339b63686fca2075824d8041781aaaf8dcf9d251
Ardager <ardager@163.com>
2019-09-27 19:51:24 +0800

    setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_des = fh.read()
+
+setuptools.setup(
+    name="g2g-kz",
+    version="0.0.1",
+    author="Ardaq",
+    author_email="ardager@163.com",
+    description="Kazakh language grapheme to phoneme converter",
+    long_description=long_des,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Ardaq/kz_g2p",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
